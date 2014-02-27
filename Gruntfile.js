f9ca0f7774f7baa97dd810ca3f3c9454b559b47b
Al Graham <alastair.graham@feedhenry.com>
2014-02-27 14:04:37 +0000

    updated gruntfile to allow flexibility

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,50 +4,50 @@ module.exports = function(grunt) {
         SENCHA_SDK: '~/touch-2.3.1',
         APP_NAME: 'fhsencha',
         copy: {
-            testingfolder: {
+            builtPackage: {
                 files: [{
                     expand: true,
-                    cwd: 'client/development/build/testing/<%=APP_NAME%>',
+                    cwd: 'client/development/build/<%= grunt.config.get("environment")%>/<%=APP_NAME%>',
                     src: ['**'],
-                    dest: 'client/default'
-                }]
-            },
-            productionfolder: {
-                files: [{
-                    expand: true,
-                    cwd: 'client/development/build/production/<%=APP_NAME%>',
-                    src: ['**'],
-                    dest: 'client/default'
+                    dest: 'client/<%= grunt.config.get("clientPackage")%>'
                 }]
             }
         },
         exec: {
-			buildtesting: {
-				command: 'cd client/development && sencha app build testing',
-				stdout: true
-			},
-			buildproduction: {
-				command: 'cd client/development && sencha app build production',
-				stdout: true
-			},
-			buildApp: {
+            build: {
+                command: 'cd client/development && sencha app build <%= grunt.config.get("environment")%>',
+                stdout: true
+            },
+			generateApp: {
 				command: 'cd <%= SENCHA_SDK %> && sencha generate app <%=APP_NAME%> '+process.cwd()+'/client/development',
 				stdout: true
 			}
 		}
     });
 
-
     //load our tasks;
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-exec');
-    console.log(process.cwd());
-    //default tasks to run
-    var target = (typeof grunt.option('target') !== 'undefined') ? grunt.option('target').toLowerCase() : 'testing';
 
-    // grunt build will auto run the 'testing' option
-    // grunt build --target=production will run production
-    grunt.registerTask('build', ['exec:build'+target+'','copy:'+target+'folder']);
-    grunt.registerTask('generate', ['exec:buildApp']);
-    // grunt.registerTask('production', ['exec:build-'+target+'','copy:'+target+'-folder']);
+    // Parse optional flags for Sencha Environment and FH Package desination
+    var environment = (typeof grunt.option('environment') !== 'undefined') ? grunt.option('target').toLowerCase() : 'testing';
+    var clientPackage = (typeof grunt.option('package') !== 'undefined') ? grunt.option('package').toLowerCase() : 'default';
+
+    // grunt build will auto run the 'testing' environment and put it in the 'default' package
+    // add --environment=[testing,package,production,native] and/or --package=<packagename> so specify targets
+    grunt.registerTask('generate', ['exec:generateApp']);
+    grunt.registerTask('build', 'Build the specified target', function() {
+        grunt.config.set('environment', environment);
+        grunt.config.set('clientPackage', clientPackage);
+        grunt.task.run('exec:build');
+        grunt.task.run('copy:builtPackage');
+        grunt.log.writeln("Building the "+environment+" environment, and moving to the "+clientPackage+" dir");
+    });
+
+
+
+
+
+
+
 };
